490194c1fa7146a54a502445807496f055a0b2a8
Shaylanism <shaundleong@gmail.com>
2025-07-15 22:06:26 +0800

    Server Setup
    Setup MongoDB with API routing, no frontend yet but using Postman to ensure API can link correctly.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,35 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/database.js";
+import User from "../models/users.js";
+
+dotenv.config();
 
 const app = express();
 
+app.use(express.json()); //Allowing the server to accept JSON data in req.body
+
+//Create
+app.post("/api/users", async (req, res) => {
+    const user = req.body; // User sends name and email.
+
+    if (!user.name || !user.email) {
+        return res.status(400).json({ success: false, message: "Name and email are required" });
+    }
+
+    const newUser = new User(user);
+    
+    try {
+        await newUser.save();
+        res.status(201).json({ success: true, message: "User created successfully", user: newUser });
+    } catch (error) {
+        console.error("Error creating user:", error.message);
+        res.status(500).json({ success: false, message: "Failed to create user"});
+    }
+});
+
 app.listen(5000, () => {
+    connectDB();
     console.log("Server is now running on https://localhost:5000");
 });
+
